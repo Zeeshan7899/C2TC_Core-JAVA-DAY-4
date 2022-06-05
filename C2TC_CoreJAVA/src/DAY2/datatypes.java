0ab5b0f3d420b80c1d344041af064aa265d4397a
ZEESHAN <ZEESHAN@LAPTOP-7LN7BL2K>
2022-06-05 12:23:03 +0530

    DAY 2

---
+++ C2TC_CoreJAVA/src/DAY2/datatypes.java
@@ -0,0 +1,15 @@
+package DAY2;
+
+public class datatypes {
+	static void test (int x) {
+		System.out.println("int");
+	}
+	static void test (long x) {
+		System.out.println("long");
+	}	
+		public static void main (String[] args) {
+			test(10000);
+			
+	}
+
+}
