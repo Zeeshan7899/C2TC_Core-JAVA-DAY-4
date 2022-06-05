029ce2908aad2d94f810dabb824f320e7ddce841
ZEESHAN <ZEESHAN@LAPTOP-7LN7BL2K>
2022-06-05 12:37:13 +0530

    DAY3

---
+++ C2TC_CoreJAVA/src/DAY3/controlstatements1.java
@@ -0,0 +1,30 @@
+package DAY3;
+
+public class controlstatements1 {
+	public class Language{
+
+		public static void main(String[] args) {
+
+		String IDE = "ECLIPSE";
+
+		if(IDE == "HLL & IL") {
+
+		if(IDE == "SELENIUM")
+
+		System.out.println("USER IS USING SELENIUM");
+
+		}else if (IDE == "ECLIPSE") {
+
+		System.out.println("USER IS USING ECLISPE");
+
+		}else if(IDE == "JAVA STUDIO") {
+
+		System.out.println("USER IS USING JAVA STUDIO");
+
+		}else {
+
+		System.out.println("THE USER IS USING AN UNIDEINTIFED IDE"); }
+
+		}}
+
+}
---
+++ C2TC_CoreJAVA/src/DAY3/controlstatements2.java
@@ -0,0 +1,22 @@
+package DAY3;
+
+public class controlstatements2 {
+	
+
+		public static void main(String[] args) {
+
+		for(int i = 5;i <= 10;i++) {
+
+		for(int j = 5;j <= 10;j++) {
+
+		System.out.println(i+" "+j);
+
+		}
+
+		}
+
+		}
+
+		}
+
+
